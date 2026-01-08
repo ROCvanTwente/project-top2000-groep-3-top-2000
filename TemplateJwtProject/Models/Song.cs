96bcc5dd335409c46baac0f40d6994be736a5073
Niek Geerligs <geerligsniek@gmail.com>
2026-01-08 15:04:30 +0100

    fix errors during build

--- TemplateJwtProject/Models/Song.cs
+++ TemplateJwtProject/Models/Song.cs
@@ -4,6 +4,10 @@ public class Song
 {
     public int SongId { get; set; }
     public string Titel { get; set; } = string.Empty;
+    public int? ReleaseYear { get; set; }
+    public string? ImgUrl { get; set; }
+    public string? Lyrics { get; set; }
+    public string? Youtube { get; set; }
     
     // Foreign key
     public int ArtistId { get; set; }
