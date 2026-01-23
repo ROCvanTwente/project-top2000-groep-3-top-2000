8ed51e039b9cccfa051c66fdc412431e6a142a1b
Niek Geerligs <144774725+0353497@users.noreply.github.com>
2026-01-23 12:39:41 +0100

    Add additional artist data fields to Artist and ArtistDto models

--- TemplateJwtProject/Models/Artist.cs
+++ TemplateJwtProject/Models/Artist.cs
@@ -7,6 +7,13 @@ public class Artist
     public string? Wiki { get; set; }
     public string? Biography { get; set; }
     public string? Photo { get; set; }
+    public string? Country { get; set; }
+    public string? CountryCode { get; set; }
+    public string? Thumbnail { get; set; }
+    public string? Style { get; set; }
+    public string? Genre { get; set; }
+    public int? FormedYear { get; set; }
+    public int? Members { get; set; }
 
     // Navigation property
     public ICollection<Song> Songs { get; set; } = new List<Song>();
